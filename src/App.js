a2469ee843013189159eae3d1069a16cf8021b79
Rachel <linrac@gmail.com>
2021-01-17 13:56:31 -0800

    positioning fixed

--- src/App.js
+++ src/App.js
@@ -10,38 +10,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="content">
-      <div className="content-box">
-        <div className="content-box-hero">
-          <HeroImage />
+    <div className="content-box">
+      <div className="content-box-hero">
+        <HeroImage />
+      </div>
+      <div className="content-box-intro">
+        <div>
+        <h1>
+          Rachel Lin is building tools for back-office financial services at <a href="http://pilot.com" target="_blank">Pilot</a>
+        </h1>
+        <h2>
+          <p>
+            I am a multidisciplinary digital designer passionate about creating tools that empower people to do their best work.
+          </p>
+          <p>
+            I have delivered intuitive tools for a variety of complex domains with high business impact for the education communication platform at Remind, prototyping enterprise applications at SAP, and navigating trips at TripIt.
+          </p>
+          <p>
+            When I’m off-duty, you can find me learning languages or experimenting in the kitchen.
+          </p>
+          <p>
+            My resume and portfolio are available upon request. Get in touch or send punny jokes to <a href="mailto:linrac@gmail.com">linrac@gmail.com</a>  👋
+          </p>
+        </h2>
         </div>
-        <div className="content-box-intro">
-
-          <div>
-          <h1>
-            Rachel Lin is building tools for back-office financial services at <a href="http://pilot.com" target="_blank">Pilot</a>
-          </h1>
-          <h2>
-            <p>
-              I am a multidisciplinary digital designer passionate about creating tools that empower people to do their best work.
-            </p>
-            <p>
-              I have delivered intuitive tools for a variety of complex domains with high business impact for the education communication platform at Remind, prototyping enterprise applications at SAP, and navigating trips at TripIt.
-            </p>
-            <p>
-              When I’m off-duty, you can find me learning languages or experimenting in the kitchen.
-            </p>
-            <p>
-              My resume and portfolio are available upon request. Get in touch or send punny jokes to <a href="mailto:linrac@gmail.com">linrac@gmail.com</a>  👋
-            </p>
-          </h2>
-          </div>
-          <div>
-            <SocialLinks />
+        <div>
+          <SocialLinks />
         </div>
-        </div>
-
-        
       </div>
     </div>
   )
