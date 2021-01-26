94c30e5b99e867b37e348f5a66b7b14a19932a04
Rachel <linrac@gmail.com>
2021-01-25 18:12:01 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -11,20 +11,19 @@ import './App.css';
 function App() {
   return (
     <div className="content-box">
-      <div className="content-box-hero">
+      {/* <div className="content-box-hero">
         <HeroImage />
-      </div>
+      </div> */}
       <div className="content-box-intro">
         <div>
         <h1>
-          Rachel Lin is building tools for financial services at <a href="http://pilot.com" target="_blank" className="link">Pilot</a>
+        Rachel Lin is designing tools that empower people to do their best work
         </h1>
         <h2>
+          <p>   
+            I am currently building tools to manage back-office financial services for startups and small businesses at <a href="http://pilot.com" target="_blank">Pilot</a>.           </p>
           <p>
-            I am a multidisciplinary digital designer passionate about creating tools that empower people to do their best work.
-          </p>
-          <p>
-            I have created enterprise and consumer experiences in a variety of complex domains, from the education communication platform at <a href="http://remind.com" target="_blank">Remind</a> to prototyping enterprise applications at <a href="http://sap.com" target="_blank">SAP</a>.
+             Previously, I led the design of advanced messaging tools for educators at <a href="http://remind.com" target="_blank">Remind</a>. I also created experiences for prototyping enterprise applications at <a href="http://sap.com" target="_blank">SAP</a> and navigating travel itineraries at <a href="http://tripit.com" target="_blank">TripIt</a>. 
           </p>
           <p>
             When I’m off-duty, you can find me learning languages or experimenting in the kitchen.
@@ -56,8 +55,8 @@ function SocialLinks() {
     <div className="social-links">
       <a href="mailto:linrac@gmail.com"><img src={email}></img></a>
       <a href="http://linkedin.com/in/linrac" target="_blank"><img src={linkedin}></img></a>
-      {/* <a href="https://www.instagram.com/rchlelle/" target="_blank"><img src={instagram}></img></a> */}
       <a href="https://dribbble.com/rachelelle" target="_blank"><img src={dribbble}></img></a>
+      <a href="https://www.instagram.com/rchlelle/" target="_blank"><img src={instagram}></img></a>
       <a href="https://twitter.com/rchlelle" target="_blank"><img src={twitter}></img></a>
     </div> 
 
